6c2956cf8f3037d6fcb3a0698a8174a7582c71da
Bhavesh Chandiwale <bhavesh_chandiwale@persistent.com>
2023-04-18 10:59:35 +0530

    Patient List Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatIconModule} from '@angular/material/icon';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatSelectModule} from '@angular/material/select';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { TableModule } from 'primeng/table';
 import { NgxEchartsModule } from 'ngx-echarts';
 import { PatientComponent } from './components/patient/patient.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { SinglePatientDetailsComponent } from './components/single-patient-details/single-patient-details.component';
 // import { GaugeChartModule } from "angular-gauge-chart";
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
- 
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -41,6 +50,16 @@ import { SinglePatientDetailsComponent } from './components/single-patient-detai
     MatSnackBarModule,
     NgxEchartsModule,
     TableModule,
+    MatCardModule,
+    MatButtonModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
+    // MatTableDataSource
   ],
   providers: [],
   bootstrap: [AppComponent]
