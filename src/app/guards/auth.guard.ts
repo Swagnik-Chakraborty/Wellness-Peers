e0956465d1354e4627dafe4462f77c3d04d0764a
unknown <kunalj040301@gmail.com>
2023-04-20 15:31:45 +0530

    final changes added

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -11,7 +11,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-      if(this.patientService.isUserLoggedIn_1){
+      if(this.patientService.isUserLoggedIn_1 || sessionStorage.getItem('isUserLoggedIn')=='true'){
         return true;
       }
       else{
