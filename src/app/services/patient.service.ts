4a68ce5bf22f3772736bc01c834e7721b60da6ea
adbalwarswapnil1 <swapnilforpsl@gmail.com>
2023-04-20 12:00:54 +0530

    Added routing and authentication

--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -1,13 +1,19 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientService {
+  isUserLoggedIn = new Subject<boolean>();
+  isUserLoggedIn_1:boolean=false;
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient) {
+    this.isUserLoggedIn.subscribe(res=>{
+      this.isUserLoggedIn_1=res;
+    })
+  }
 
   getPieChartData():Observable<any>{
     return this.http.get<any>('http://127.0.0.1:5000/piedata');
